2ae468a6d0632d4a60c756efc11f8dff46a43d1a
chekirko <victorche59@gmail.com>
2022-09-28 23:36:12 +0300

    додає рефакторинг апп

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,5 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-// import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import { StyledApp } from './App.styled';
 import { Searchbar } from '../Searchbar/Searchbar';
 import { ImageGallery } from '../ImageGallery/ImageGallery';
@@ -8,84 +7,80 @@ import { Modal } from 'components/Modal/Modal';
 import { Button } from 'components/Button/Button';
 import { Loader } from 'components/Loader/Loader';
 
-export class App extends Component {
-  KEY = '20889141-879292f7de0589dc14ea8950f';
+export const App = function () {
+  const KEY = '20889141-879292f7de0589dc14ea8950f';
 
-  state = {
-    images: [],
-    loading: false,
-    error: null,
-    name: '',
-    page: 1,
-    modalContent: '',
-  };
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [name, setName] = useState('');
+  const [page, setPage] = useState(1);
+  const [modalContent, setModalContent] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevName = prevState.name;
-    const { name, page } = this.state;
-    if (prevName !== name) {
-      this.fetchImages(name, page).then(response => {
-        this.setState({ images: response, page: page + 1, loading: false });
-      });
+  useEffect(() => {
+    if (name === '') {
+      return;
     }
-  }
+    fetchImages(name, page).then(response => {
+      setImages(response);
+      setPage(prev => {
+        return prev + 1;
+      });
+    });
+  }, [name]);
 
-  handleChangeState = name => {
-    this.setState({ name: name, page: 1, loading: true });
+  const handleChangeState = name => {
+    setName(name);
+    setPage(1);
+    setLoading(true);
   };
 
-  fetchImages = async (name, page) => {
+  const fetchImages = async (name, page) => {
     try {
       const response = await axios.get(
-        `https://pixabay.com/api/?q=${name}&page=${page}&key=${this.KEY}&image_type=photo&orientation=horizontal&per_page=12`
+        `https://pixabay.com/api/?q=${name}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
       );
       return response.data.hits;
     } catch (error) {
-      this.setState({ error: `Sorry, we have an error: ${error}` });
+      setError(`Sorry, we have an error: ${error}`);
       console.error(error);
     } finally {
-      this.setState({ loading: false });
+      setLoading(false);
     }
   };
 
-  handleLoadMoreBtn = () => {
-    const { name, page } = this.state;
-    this.setState({ loading: true });
-    this.fetchImages(name, page).then(response => {
-      this.setState(prevState => ({
-        images: [...prevState.images, ...response],
-        page: prevState.page + 1,
-      }));
+  const handleLoadMoreBtn = () => {
+    setLoading(true);
+    fetchImages(name, page).then(response => {
+      setImages(prev => {
+        return [...prev, ...response];
+      });
+      setPage(prev => {
+        return prev + 1;
+      });
     });
   };
 
-  closeModal = () => {
-    this.setState({
-      modalContent: '',
-    });
+  const closeModal = () => {
+    setModalContent('');
   };
 
-  openModal = largeImg => {
-    this.setState({
-      modalContent: largeImg,
-    });
+  const openModal = largeImg => {
+    setModalContent(largeImg);
   };
 
-  render() {
-    const { images, loading, error, modalContent } = this.state;
-    return (
-      <StyledApp>
-        <Searchbar onSubmit={this.handleChangeState}></Searchbar>
-        {error && <h2>{error}</h2>}
-        <ImageGallery images={images} onClick={this.openModal}></ImageGallery>
-        {loading && <Loader />}
-        {images.length > 0 && <Button onClick={this.handleLoadMoreBtn} />}
-        {modalContent && (
-          <Modal onClose={this.closeModal}>
-            <img src={modalContent} alt="" />
-          </Modal>
-        )}
-      </StyledApp>
-    );
-  }
-}
+  return (
+    <StyledApp>
+      <Searchbar onSubmit={handleChangeState}></Searchbar>
+      {error && <h2>{error}</h2>}
+      <ImageGallery images={images} onClick={openModal}></ImageGallery>
+      {loading && <Loader />}
+      {images.length > 0 && <Button onClick={handleLoadMoreBtn} />}
+      {modalContent && (
+        <Modal onClose={closeModal}>
+          <img src={modalContent} alt="" />
+        </Modal>
+      )}
+    </StyledApp>
+  );
+};
